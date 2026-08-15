d84a07684c9e9bc29d83f2df2480cc90d60a3b04
rachityadav111 <ry05052004@gmail.com>
2026-08-15 14:24:32 +0530

    reverse nm

---
+++ ph1/num.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,r,s;
+    cin>>n;
+    while(n>0){
+        r=n%10;
+        s=s*10+r;
+        n=n/10;
+    }
+    cout<<s;
+}
---
+++ ph1/starting.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"aliye re aliye";
+    return 0;
+}
