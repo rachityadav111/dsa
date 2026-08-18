a86f1d7e0fb93578a91d83561b5efdf6e0a769d9
rachityadav111 <ry05052004@gmail.com>
2026-08-19 04:54:09 +0530

    rev

---
+++ ph1/arrayl.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+void oe(int arr[],int n){
+    int o=0,e=0;
+    for(int i=0;i<n;i++){
+        if(arr[i]%2==0) o++;
+        else e++;
+    }
+    cout<<"odd elements in array"<<e<<endl;
+    cout<<"even elements in array"<<o<<endl;
+}
+void rev(int arr[],int n){
+    for(int i=n-1;i>=0;i--){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+int sl(int arr[],int n){
+    int l,s;
+    l=arr[0];
+    for(int i=0;i<n;i++){
+        if(l<arr[i]){
+            l=arr[i];
+        }
+    }
+    for(int i=0;i<n;i++){
+        if(l>arr[i] && ){
+          s=arr[i];
+            }
+    }
+    return s;
+}
+int main(){
+    int n=0,r;
+    cout<<"enter the nmber of elements ";
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cout<<"enter ayyar elements";
+        cin>>arr[i];
+    }
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+    oe(arr,n);
+    cout<<endl;
+    rev(arr,n);
+    cout<<endl;
+    r=sl(arr,n);
+    cout<<"second largest element = "<<r;
+}
